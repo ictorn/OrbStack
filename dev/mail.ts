aeebf0dfb6834049c6a8646b7d95ecc7ffaa9593
Tomasz Sądej <tomasz@tbx.pl>
2024-05-15 18:16:52 +0200

    refactor: mailhog -> mailpit

--- dev/mail.ts
+++ dev/mail.ts
@@ -1,11 +1,29 @@
-import { Pod, Service } from "@pulumi/kubernetes/core/v1";
+import { Pod, Service, Secret } from "@pulumi/kubernetes/core/v1";
 import { Config } from "@pulumi/pulumi";
+import {execSync} from "child_process";
+import {readFileSync, unlinkSync} from "fs";
 export default () => {
     const config = new Config
     const mail = config.requireObject<{
         image: string
         tag: string
     }>("mail");
+
+    execSync(`mkcert -cert-file tls.crt -key-file tls.key 127.0.0.1 ::1 localhost "mail.svc.cluster.local"`);
+
+    const ssl: Secret = new Secret("mail.ssl", {
+        metadata: {
+            name: "mail.ssl"
+        },
+        type: "tls",
+        data: {
+            "tls.crt": readFileSync('tls.crt', 'base64'),
+            "tls.key": readFileSync('tls.key', 'base64')
+        }
+    });
+
+    unlinkSync('tls.crt');
+    unlinkSync('tls.key');
 
     const pod: Pod = new Pod('mail', {
         metadata: {
@@ -15,16 +33,43 @@ export default () => {
                 "version": mail.tag
             },
             annotations: {
-                "kubectl.kubernetes.io/default-container": "mailhog",
+                "kubectl.kubernetes.io/default-container": "server",
                 "pulumi.com/patchForce": "true"
             }
         },
         spec: {
             os: { name: "linux" },
+            volumes: [{
+                name: "ssl",
+                secret: {
+                    secretName: ssl.metadata.name
+                }
+            }],
             containers: [{
-                name: "mailhog",
+                name: "server",
                 image: mail.image + ":" + mail.tag,
                 imagePullPolicy: "IfNotPresent",
+                env: [
+                    {
+                        name: "MP_UI_TLS_CERT",
+                        value: "/cert/tls.crt"
+                    },
+                    {
+                        name: "MP_UI_TLS_KEY",
+                        value: "/cert/tls.key"
+                    },
+                    {
+                        name: "MP_SMTP_DISABLE_RDNS",
+                        value: "true"
+                    }
+                ],
+                volumeMounts: [
+                    {
+                        mountPath: "/cert",
+                        name: "ssl",
+                        readOnly: true
+                    }
+                ],
                 ports: [
                     {
                         name: "webui",
@@ -37,6 +82,8 @@ export default () => {
                 ]
             }]
         }
+    }, {
+        dependsOn: ssl
     })
 
     const service: Service = new Service("mail", {
@@ -46,7 +93,7 @@ export default () => {
             ports: [
                 {
                     name: "webui",
-                    port: 80,
+                    port: 443,
                     targetPort: 8025
                 },
                 {
